dfb18281e0e9c6e5e169807a051ebf01fec9efa3
MD ABDUL HADY <hady.onnorokom@gmail.com>
2023-02-14 17:07:29 +0600

    Working on Phone screen

---
+++ lib/Activity/Common/Constant.dart
@@ -0,0 +1,2 @@
+
+const double progress = 0.1;
--- lib/Activity/PhoneAuthentication.dart
+++ lib/Activity/PhoneAuthentication.dart
@@ -1,16 +1,74 @@
-
+import 'package:final_design/Activity/Common/Constant.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PhoneAuthentication extends StatefulWidget {
+class PhoneAuthentication extends ConsumerStatefulWidget {
   const PhoneAuthentication({Key? key}) : super(key: key);
 
   @override
-  State<PhoneAuthentication> createState() => _PhoneAuthenticationState();
+  ConsumerState<PhoneAuthentication> createState() =>
+      _PhoneAuthenticationState();
 }
 
-class _PhoneAuthenticationState extends State<PhoneAuthentication> {
+class _PhoneAuthenticationState extends ConsumerState<PhoneAuthentication> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.white));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      // appBar: AppBar(
+      //   bottom: PreferredSize(
+      //     preferredSize: Size.fromHeight(-50),
+      //     child: AppBar(
+      //       title: const Text("Dummy Title"),
+      //     ),
+      //   ),
+      // ),
+      body: Container(
+        margin: const EdgeInsets.only(top: 45),
+        child: Column(
+          children: <Widget>[
+            const LinearProgressIndicator(
+              backgroundColor: Colors.grey,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+              value: progress,
+            ),
+
+            Container(
+              height: 40,
+              alignment: Alignment.center,
+              child:Stack(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back, color: Colors.black),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                  const Text("Back",
+                      style: TextStyle(color: Colors.grey, fontSize: 16))
+                ],
+              )
+            ),
+
+            const Text("My Phone number is",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black)),
+            const Text(
+                "When you 'continue', Finally App will send you a text with verification code. Message and data rates may apply",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, color: Colors.black)),
+          ],
+        ),
+      ),
+    );
   }
 }
