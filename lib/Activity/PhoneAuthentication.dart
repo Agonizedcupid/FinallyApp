bba2ae1a43391f3e201218ee020efaabf4ff2428
MD ABDUL HADY <hady.onnorokom@gmail.com>
2023-02-15 19:13:52 +0600

    Working on PhoneNumberPicker

--- lib/Activity/PhoneAuthentication.dart
+++ lib/Activity/PhoneAuthentication.dart
@@ -1,4 +1,5 @@
 import 'package:final_design/Activity/Common/Constant.dart';
+import 'package:final_design/Resources/Margins.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -24,50 +25,66 @@ class _PhoneAuthenticationState extends ConsumerState<PhoneAuthentication> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   bottom: PreferredSize(
-      //     preferredSize: Size.fromHeight(-50),
-      //     child: AppBar(
-      //       title: const Text("Dummy Title"),
-      //     ),
-      //   ),
-      // ),
-      body: Container(
-        margin: const EdgeInsets.only(top: 45),
-        child: Column(
-          children: <Widget>[
-            const LinearProgressIndicator(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(top: 45),
+            child: const LinearProgressIndicator(
               backgroundColor: Colors.grey,
               valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
               value: progress,
             ),
+          ),
 
-            Container(
-              height: 40,
-              alignment: Alignment.center,
-              child:Stack(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.arrow_back, color: Colors.black),
-                    onPressed: () => Navigator.of(context).pop(),
-                  ),
-                  const Text("Back",
-                      style: TextStyle(color: Colors.grey, fontSize: 16))
-                ],
-              )
+
+          Container(
+            margin: const EdgeInsets.only(top: 10, bottom: 25,left: 5, right: rightMargin),
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back_ios, color: Colors.grey),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 0),
+                      child: const Text("Back",
+                          style: TextStyle(color: Colors.grey, fontSize: 18)),
+                    )
+                  ],
+                ),
+              ],
+            )
+          ),
+
+
+          Container(
+            margin: const EdgeInsets.only(left: leftMargin, right: rightMargin),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget> [
+                const Text("My Phone number is",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
+                Container(
+                  margin: const EdgeInsets.only(top: 25),
+                  child: const Text(
+                      "When you 'continue', Finally App will send you a text with verification code. Message and data rates may apply",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 18, color: Colors.black)),
+                ),
+
+              ],
             ),
-
-            const Text("My Phone number is",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black)),
-            const Text(
-                "When you 'continue', Finally App will send you a text with verification code. Message and data rates may apply",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, color: Colors.black)),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
